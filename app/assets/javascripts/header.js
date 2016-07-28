5780dee4caa42fa7eb3fb90d555acfa5912e94df
jwgurley <jwgurley@ncsu.edu>
2016-07-28 17:19:37 -0400

    working on identify responsiveness

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -3,6 +3,7 @@
 var loadPageJS = function() {
 
   var infoTopPosition = 0;
+  var menuLeftPosition = 0;
 
   // Show page information/instructions on icon click
   $(".header-page-icon").on("click", toggleDropDown);
@@ -32,21 +33,24 @@ var loadPageJS = function() {
 
   // Show menu and blur underlying page
   function toggleMenu() {
+    console.log(menuLeftPosition);
     $(".page-with-header").toggleClass("blur");
-
     $(".menu").animate({
-      "left": infoTopPosition
+      "left": menuLeftPosition
     }, {
       duration: 500,
       done: function() {
+
         // After animation finishes set position for next toggle and add event
         //  listener for blured page
-        if (infoTopPosition === 0) {
-          infoTopPosition = $(".info-drop-down").outerHeight() * -1;
-          $(".page-with-header").on("click", toggleDropDown);
+        if (menuLeftPosition === 0) {
+          menuLeftPosition = $(".menu").outerWidth() * -1;
+          $(".page-with-header").on("click", toggleMenu);
+          $(".header-page-icon").off("click", toggleDropDown);
         } else {
-          infoTopPosition = 0;
-          $(".page-with-header").off("click", toggleDropDown);
+          menuLeftPosition = 0;
+          $(".page-with-header").off("click", toggleMenu);
+          $(".header-page-icon").on("click", toggleDropDown);
         }
       }
     });
@@ -55,13 +59,24 @@ var loadPageJS = function() {
   // Wait until window is loaded and then attach resize event, this prevents
   //  resize issues that occur during the page load
   $(window).on("load", function() {
+
     // Handle info drop down position on window resize
     $(window).on("resize", function() {
-      // Set next top position variable, set top position, remove event
-      //  listener for underlying page, and remove blur
+
+      // Check if header is present and return if not
+      if ($(".header").length < 1) { return; }
+
+      // Set next top and left position variable and local div heights
       infoTopPosition = 0;
-        console.log($(".info-drop-down").outerHeight());
+      menuLeftPosition = 0;
+      var dropDownHeight = $(".info-drop-down").outerHeight();
+      // Move menu and drop-down to initial hidden positions, remove
+      //  event listener for underlying page, and remove blur
+      if ($(".menu").css("left") === "0px") {
+        $(".header-page-icon").on("click", toggleDropDown);
+      }
       $(".info-drop-down").css("top", $(".info-drop-down").outerHeight() * -1);
+      $(".menu").css("left", $(".menu").outerWidth() * -1);
       $(".page-with-header").off("click", toggleDropDown);
       $(".page-with-header").removeClass("blur");
     });
