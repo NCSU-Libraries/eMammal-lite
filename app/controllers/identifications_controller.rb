e46c1b0c9f484fb2b5181603cd6543f8ec7f8f8a
jwgurley <jwgurley@ncsu.edu>
2016-10-03 11:13:00 -0400

    optimized image persistence

--- app/controllers/identifications_controller.rb
+++ app/controllers/identifications_controller.rb
@@ -1,15 +1,16 @@
 class IdentificationsController < ApplicationController
+  before_action :check_for_skip, only: [:new]
 
   def new
     if user_signed_in?
       @identification = Identification.new
       @photo = Photo.find_by_id(session[:last_photo]) ||
         Photo.order("RAND()").first
-      session[:last_photo] = @photo.id
 
       # Get the 'wrong' animals from the two_similar_animals function
       getSimilarAnimals
 
+      session[:last_photo] = @photo.id
       respond_to do |format|
         format.html { render new_identification_path }
         format.js
@@ -23,7 +24,6 @@ class IdentificationsController < ApplicationController
   def create
     @identification = Identification.new(selection_params)
     @identification.save
-    session.delete(:last_photo)
     # If user skips id generate new photo and animal and proceed to next card
 
     respond_to do |format|
@@ -42,6 +42,15 @@ end
       params.require(:identification)
         .permit(:photo_id, :user_identification,
           :correct_identification, :user_id)
+    end
+
+    def check_for_skip
+        session.delete(:last_photo) if request.format.symbol == :js
+        puts request.referrer
+    end
+
+    def remove_last_photo
+      session.delete(:last_photo)
     end
 
     def getSimilarAnimals
