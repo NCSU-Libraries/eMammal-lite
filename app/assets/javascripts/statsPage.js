f6e91848a39fa0119ffcd1986e3e978371cf08a9
jwgurley <jwgurley@ncsu.edu>
2016-10-19 17:55:34 -0400

    stats page graphics for score accuracy done

--- app/assets/javascripts/statsPage.js
+++ app/assets/javascripts/statsPage.js
@@ -2,6 +2,77 @@ function loadPageJS() {
   if ($(".stats-page ").length > 0) { statsPage(); }
 
   function statsPage() {
+    var stats = $(".stats-page").data("url");
+
+    // Score stats
+    function makeScoreBarGraph() {
+      $(".percentage-bar")
+        .attr("width", stats.correct / stats.total * 100 + "%");
+    }
+    makeScoreBarGraph();
+
+    // Accuracy stats
+    function makeAccuracyDonut() {
+      var width = $(".accuracy-donut").width();
+      var height = $(".accuracy-donut").height();
+
+      // Create the initial arc parameters
+      var arc = d3.arc()
+       .innerRadius(height / 2 - 13)
+       .outerRadius(height / 2 - 5);
+
+      // Use d3.pie to set up reading of data to calculate inner/outer arc radius
+      var piePieceArcs = d3.pie().value(function(d) { return d; });
+
+      // Create group and move to center of div
+      var donutChart = d3.select(".accuracy-donut")
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      // Add data to groups that will hold the donut pieces and text
+      var donutPiece = donutChart.selectAll("g")
+          .data(piePieceArcs([(stats.total - stats.correct), stats.correct]))
+          .enter().append("g")
+          .attr("class", "donut-piece");
+
+      // Create paths of the pieces using the arc parameters
+      donutPiece.append("path")
+        .attr("class", function(d, i) { return "stat-color-" + i; })
+        .attr("d", arc);
+
+      donutChart.append("text")
+        .text(Math.ceil(stats.correct / stats.total * 100) + "%")
+        .attr("class", "xl-header bold-text donut-text");
+
+      d3.select(".accuracy-number").text(Math.ceil(stats.correct / stats.total * 100) + "%");
+
+    }
+    makeAccuracyDonut();
+
+    // Top identified animals stats
+    function makeTopIdentifiedBarGraph() {
+      var width = $(".top-bar").width();
+      var height = $(".top-bar").height();
+      var topPadding = 20;
+      var animalCounts = d3.values(stats.animals);
+
+      var bar = d3.select(".top-bar").selectAll("g")
+          .data(animalCounts)
+          .enter().append("g");
+
+      bar.append("rect")
+      	.attr("x", function(d, i) { return i * width / 3; })
+      	.attr("y",  function(d) {
+          return height + topPadding - (d / Math.max(...animalCounts) * height);
+        })
+      	.attr("width", width / 3)
+      	.attr("height", height)
+        .attr("class", "stat-color-0");
+
+    }
+    makeTopIdentifiedBarGraph();
+
+    // Set up the ui and animation of the stats buttons
     var accordionBtns = $(".stats-group-header");
 
     accordionBtns.on("click", function() {
@@ -35,6 +106,7 @@ function loadPageJS() {
         );
       }
     });
+    console.log(d3.values(stats.animals));
     console.log("loaded js for stats page");
   }
 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,17 @@ class User < ApplicationRecord
   # Return the stats for a user
   def stats
    total_ids = self.identifications
+   topAnimals = total_ids.where("correct_identification = true")
+    .group("user_identification")
+    .count
+    .sort_by{|key, val| val}
+    .reverse.slice(0,3)
    {
      :correct => total_ids.where("correct_identification = true").count,
      :incorrect => total_ids.where("user_identification IS NOT NULL AND correct_identification = false").count,
      :skipped => total_ids.where("user_identification IS NULL").count,
-     :total => total_ids.count
+     :total => total_ids.count,
+     :animals => topAnimals.map{|key,val| [Animal.find(key).name, val]}.to_h
    }
   end
 end
